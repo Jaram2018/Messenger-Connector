e6ae42da28fb965cc620528d91b15e7d16ce5bd1
ByeongGil-Jung <jbkcose@gmail.com>
2018-09-09 13:51:03 +0900

    [18-09-09] :: init Spring Boot - init Spring Boot

---
+++ messenger-connector/src/main/java/com/msg/messengerconnector/MessengerConnectorApplication.java
@@ -0,0 +1,12 @@
+package com.msg.messengerconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessengerConnectorApplication.class, args);
+    }
+}
---
+++ messenger-connector/src/test/java/com/msg/messengerconnector/MessengerConnectorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.msg.messengerconnector;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MessengerConnectorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

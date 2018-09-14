7b217e273ecd67b519bb9194e0229d51f800a208
ByeongGil-Jung <jbkcose@gmail.com>
2018-09-15 03:46:38 +0900

    [18-09-15] :: Modifying KakaotalkController - Modifying KakaotalkController - Adding interface of messenger controllers

---
+++ messenger-connector/src/main/java/com/bkmc/messengerconnector/communicate/IController.java
@@ -0,0 +1,12 @@
+package com.bkmc.messengerconnector.communicate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project: Messenger-Connector
+ * ===========================================
+ * User: ByeongGil Jung
+ * Date: 2018-09-15
+ * Time: 오전 3:35
+ */
+public interface IController {
+}
--- messenger-connector/src/main/java/com/bkmc/messengerconnector/communicate/kakaotalk/KakaotalkController.java
+++ messenger-connector/src/main/java/com/bkmc/messengerconnector/communicate/kakaotalk/KakaotalkController.java
@@ -1,30 +1,37 @@
 package com.bkmc.messengerconnector.communicate.kakaotalk;
 
+import com.bkmc.messengerconnector.communicate.IController;
+import com.bkmc.messengerconnector.config.Messenger;
 import com.bkmc.messengerconnector.message.BasicMessage;
 import com.bkmc.messengerconnector.message.Message;
 import com.bkmc.messengerconnector.messagehub.EntireMessageHub;
-import com.bkmc.messengerconnector.config.Messenger;
+import com.bkmc.messengerconnector.messagehub.KakaotalkMessageHub;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by IntelliJ IDEA.
- * Project: Messenger-Connector
+ * Project: communicate-Connector
  * ===========================================
  * User: ByeongGil Jung
  * Date: 2018-09-09
- * Time: 오후 5:25
+ * Time: 오후 5:19
  */
 @RestController
-public class KakaotalkReceiver {
+public class KakaotalkController implements IController {
 
     @Autowired
     EntireMessageHub entireMessageHub;
 
-    @RequestMapping("/kakaotalkReceiver")
+    @Autowired
+    KakaotalkMessageHub kakaotalkMessageHub;
+
+    @RequestMapping("/kakaotalk/receive")
     public String kakaotalkReceiver(HttpServletRequest request) {
         String channelFrom = request.getParameter("channel_from");
         String sender = request.getParameter("sender");
@@ -40,5 +47,28 @@ public class KakaotalkReceiver {
         System.out.println("Success inserting kakaotalk message to messagehub");
 
         return "Success";
+    }
+
+    @RequestMapping("/kakaotalk/send")
+    public ArrayList getKakaotalkMessages() {
+        ArrayList<Message> msgData = kakaotalkMessageHub.pollAllMessage();
+        return formattingProcess(msgData);
+    }
+
+    private ArrayList formattingProcess(ArrayList<Message> msgDTOs) {
+        ArrayList<HashMap> msgList = new ArrayList<>();
+        HashMap<String, String> msg = new HashMap<>();
+
+        for (Message message : msgDTOs) {
+            msg.put("messenger", message.getMessenger().name());
+            msg.put("channel_from", message.getChannelFrom());
+            msg.put("sender", message.getSender());
+            msg.put("context", message.getContext());
+            msg.put("date", message.getDate().toString());
+
+            msgList.add(msg);
+        }
+
+        return msgList;
     }
 }
